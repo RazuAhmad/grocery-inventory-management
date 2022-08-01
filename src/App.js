446ee2eaba170f79762ee362c27a296f8f640253
RazusWebsite <abdurrahmanrazu2000@gmail.com>
2022-08-02 02:36:26 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../src/Components/Navbar/Navbar";
+import Banner from "./Components/Banner/Banner";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <br />
+      <Banner />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+/*
+
+
+
+*/
+const Banner = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.ibb.co/6Rx0xkv/pexels-carlo-martin-alcordo-2449665.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.ibb.co/6mLHj4r/pexels-photomix-company-868110.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.ibb.co/djVgv47/pexels-matheus-cenali-2733918.jpg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default Banner;
